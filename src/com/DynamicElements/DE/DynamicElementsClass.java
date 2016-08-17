93ebc10b64865e3cb40622a20451365b1ba65df3
cnspraveen <cns.praveen@gmail.com>
2016-08-17 12:17:00 +0530

    Update DynamicElementsClass.java

--- src/com/DynamicElements/DE/DynamicElementsClass.java
+++ src/com/DynamicElements/DE/DynamicElementsClass.java
@@ -14,6 +14,7 @@ public void DynamicElements()
 	WebDriver driver = new FirefoxDriver();
 	driver.get("https://mail.google.com");
 	Reporter.log("Opening Gmail in firefox", true);
+        Reporter.log("Changes from github to Eclipse" +  "  " + "Gmail in Firfrox", true);	
 	//Reporter.log("DynamicElements", true);
 	
 	
@@ -21,6 +22,7 @@ public void DynamicElements()
     WebDriver driver2 = new ChromeDriver();
     driver2.navigate().to("https://github.com/");
     Reporter.log("Opening Github in Chrome", true);
+    Reporter.log("Changes from github to Eclipse" +  "  " + "Github in Chrome", true);
     driver.close();
     driver2.close();
 }
